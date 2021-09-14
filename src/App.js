5664580493f7ab59079bacb4c9de0d1100bccc74
Grant Harris <grant.harris@ibm.com>
2021-09-13 19:55:05 -0500

    Rearrange imports

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import hobbies from './hobbyData.js'
 import Grid from '@material-ui/core/Grid'
 import { Paper } from '@material-ui/core';
-import hobbies from './hobbyData.js'
 
 function App() {
   return (
     <>
       <Grid container>
-        {hobbies.map((hobby, i) => 
+        {hobbies.map((hobby, i) => {
+          return (
             <Grid item xs={12} sm={6} med={3}>
-            <Paper>
-              Hello!  My name is {hobby.name}.  My favorite hobby is {hobby.interests} and my favorite food is {hobby.favoriteFood}
-            </Paper>
-          </Grid>
-        )}
+              <Paper>
+                Hello!  My name is {hobby.name}.  My favorite hobby is {hobby.interests} and my favorite food is {hobby.favoriteFood}
+              </Paper>
+            </Grid>
+          )
+        })}
       </Grid>
     </>
   );
