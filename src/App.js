a6da275dc353b76ded0fba5b3efcf0add09a57e9
Grant Harris <grant.harris@ibm.com>
2021-09-13 19:34:16 -0500

    mapped over hobbyData.js to create components

--- src/App.js
+++ src/App.js
@@ -2,31 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 import Grid from '@material-ui/core/Grid'
 import { Paper } from '@material-ui/core';
+import hobbies from './hobbyData.js'
 
 function App() {
   return (
     <>
       <Grid container>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>
-            Francene
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>
-            Marina
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>
-            Jorge
-          </Paper>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3}>
-          <Paper>
-            Grant
-          </Paper>
-        </Grid>
+        {hobbies.map((hobby, i) => 
+            <Grid item xs={12} sm={6} med={3}>
+            <Paper>
+              Hello!  My name is {hobby.name}.  My favorite hobby is {hobby.interests} and my favorite food is {hobby.favoriteFood}
+            </Paper>
+          </Grid>
+        )}
       </Grid>
     </>
   );
---
+++ src/hobbyData.js
@@ -0,0 +1,24 @@
+const hobbies = [
+    {
+        name: 'Grant',
+        interests: 'Skateboarding',
+        favoriteFood: 'Red Curry1'
+    },
+    {
+        name: 'Marina',
+        interests: 'Skateboarding',
+        favoriteFood: 'Red Curry2'
+    },
+    {
+        name: 'Jorge',
+        interests: 'Skateboarding',
+        favoriteFood: 'Red Curry3'
+    },
+    {
+        name: 'Francine',
+        interests: 'Skateboarding',
+        favoriteFood: 'Red Curry4'
+    }
+];
+
+export default hobbies;
